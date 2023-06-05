ec4357e284577886ef81437f425edf097514e27b
rahul shah <rshah@shoppersdrugmart.ca>
2023-06-05 18:56:46 +0530

    temp commit

--- src/main/java/com/springboot/ws/SpringBootApplicationMain.java
+++ src/main/java/com/springboot/ws/SpringBootApplicationMain.java
@@ -1,12 +1,24 @@
 package com.springboot.ws;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringBootApplicationMain {
 
+    public static Logger logger = LoggerFactory.getLogger(SpringBootApplicationMain.class);
+
+    @PostConstruct
+    public void init() {
+        logger.info("this is init method");
+    }
+
     public static void main(String [] ar) {
+        logger.info("Application executed");
         SpringApplication.run(SpringBootApplicationMain.class);
     }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,19 @@
+import com.springboot.ws.SpringBootApplicationMain;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 public class Test {
 
-    @org.junit.jupiter.api.Test
-    public void dropWhile() {
+    public static Logger logger = LoggerFactory.getLogger(Test.class);
 
+    @org.junit.jupiter.api.Test
+    public void test() {
+        logger.info("Test case executing");
+        Assertions.assertEquals(true,true);
     }
 
 }
